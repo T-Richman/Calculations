f2b82c1b1c1c2ff0ff35a525d5029c824458fb42
A. Richards <neofiguresteam@gmail.com>
2026-06-01 18:42:32 -0400

    Update Stat_Calc_1.java
    Added Java Swing GUI

--- Stat_Calc_1.java
+++ Stat_Calc_1.java
@@ -1,5 +1,127 @@
-package calculations;
+package calculators;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+
+class SC1Page implements ActionListener{
+	private JFrame mainFrame = new JFrame();
+	private JPanel inPanel = new JPanel();
+	private JPanel outPanel = new JPanel();
+	private JLabel meanXLabel = new JLabel();
+	private JLabel meanYLabel = new JLabel();
+	private JLabel standevXLabel = new JLabel();
+	private JLabel standevYLabel = new JLabel();
+	private JLabel corrCoeffLabel = new JLabel();
+	private JLabel slopeLabel = new JLabel();
+	private JLabel yIntLabel = new JLabel();
+	private JTextField valuesX = new JTextField();
+	private JTextField valuesY = new JTextField();
+	private JButton submitButton = new JButton("Submit");
+	private int dist_in;
+	private int dist_out;
+	private int text_height;
+	private int max_char;
+	
+	public SC1Page() {
+		dist_in = 40;
+		dist_out = 20;
+		text_height = 20;
+		max_char = 250;
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(640, 320);
+		mainFrame.setVisible(true);
+		mainFrame.setLayout(null);
+		
+		inPanel.setBounds(0,0,mainFrame.getWidth()/2,mainFrame.getHeight());
+		inPanel.setLayout(null);
+		outPanel.setBounds(inPanel.getWidth(),0,mainFrame.getWidth()/2,mainFrame.getHeight());
+		outPanel.setLayout(null);
+		
+		valuesX.setBounds(20, 20, max_char, text_height);
+		valuesX.setToolTipText("Enter x-values");
+		valuesY.setBounds(20, valuesX.getY()+dist_in, max_char, text_height);
+		valuesY.setToolTipText("Enter y-values");
+		
+		submitButton.setBounds(20, valuesY.getY()+dist_in, 100, 40);
+		submitButton.setText("Submit");
+		submitButton.setFocusable(false);
+		submitButton.addActionListener(this);
+		
+	}
+	
+	public void setup() {
+		
+		inPanel.add(valuesX);
+		inPanel.add(valuesY);
+		inPanel.add(submitButton);
+		
+		outPanel.add(meanXLabel);
+		outPanel.add(meanYLabel);
+		outPanel.add(standevXLabel);
+		outPanel.add(standevYLabel);
+		outPanel.add(corrCoeffLabel);
+		outPanel.add(slopeLabel);
+		outPanel.add(yIntLabel);
+		mainFrame.add(inPanel);
+		mainFrame.add(outPanel);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==submitButton) {
+			String[] x = valuesX.getText().split(",");
+			LinkedList<Double> x_val = new LinkedList<Double>();
+			for(int i=0;i<x.length;i++) {
+				x_val.add(Double.valueOf(x[i]));
+			}
+			String[] y = valuesY.getText().split(",");
+			LinkedList<Double> y_val = new LinkedList<Double>();
+			for(int i=0;i<y.length;i++) {
+				y_val.add(Double.valueOf(y[i]));
+			}
+			
+			double x_bar = Stat_Calc_1.mean_calc(x_val);
+			double y_bar = Stat_Calc_1.mean_calc(y_val);
+			draw_mean(x_bar,y_bar);
+			double standev_x = Stat_Calc_1.stan_dev_calc(x_val,x_bar);
+			double standev_y = Stat_Calc_1.stan_dev_calc(y_val,y_bar);
+			draw_standev(standev_x,standev_y);
+			double corr_coeff = Stat_Calc_1.corr_coeff_calc(x_val, x_bar, y_val, y_bar);
+			double slope = Stat_Calc_1.slope_calc(corr_coeff, standev_x, standev_y);
+			double y_int = Stat_Calc_1.y_int_calc(slope, x_bar, y_bar);
+			draw_misc(corr_coeff,slope,y_int);
+			
+		}
+	}
+	
+	public void draw_mean(double x, double y) {
+		meanXLabel.setText("Mean X-Values: "+x);
+		meanXLabel.setBounds(20, 20, max_char, text_height);
+		meanYLabel.setText("Mean Y-Values: "+y);
+		meanYLabel.setBounds(20, meanXLabel.getY()+dist_out, max_char, text_height);
+	}
+	
+	public void draw_standev(double x, double y) {
+		standevXLabel.setText("Standard Deviation X-Values: "+x);
+		standevXLabel.setBounds(20, meanYLabel.getY()+dist_out, max_char, text_height);
+		standevYLabel.setText("Standard Deviation Y-Values: "+y);
+		standevYLabel.setBounds(20, standevXLabel.getY()+dist_out, max_char, text_height);
+	}
+	
+	public void draw_misc(double x, double y, double z) {
+		corrCoeffLabel.setText("Correlation Coefficient: "+x);
+		corrCoeffLabel.setBounds(20, standevYLabel.getY()+dist_out, max_char, text_height);
+		slopeLabel.setText("Slope: "+y);
+		slopeLabel.setBounds(20, corrCoeffLabel.getY()+dist_out, max_char, text_height);
+		yIntLabel.setText("Y-Intercept: "+z);
+		yIntLabel.setBounds(20, slopeLabel.getY()+dist_out, max_char, text_height);
+	}
+	
+}
 
 public class Stat_Calc_1 {
 
@@ -59,50 +181,7 @@ public class Stat_Calc_1 {
 	}
 	
 	public static void main(String[] args) {
-		Scanner scan_1 = new Scanner(System.in);
-		int input1=-1;
-		while(input1!=0) {
-			scan_1.reset();
-			System.out.println("Enter the sample size (n): ");
-			int n=scan_1.nextInt();
-			scan_1.reset();
-			
-			LinkedList<Double> x_val = new LinkedList<Double>();
-			LinkedList<Double> y_val = new LinkedList<Double>();
-			System.out.println("Enter the x-values: ");
-			for(int i=0;i<n;i++) {
-				x_val.add(scan_1.nextDouble());
-			}
-			scan_1.reset();
-			
-			System.out.println("Enter the y-values: ");
-			for(int i=0;i<n;i++) {
-				y_val.add(scan_1.nextDouble());
-			}
-			scan_1.reset();
-			
-			double x_bar = mean_calc(x_val);
-			double y_bar = mean_calc(y_val);
-			double stan_dev_x = stan_dev_calc(x_val, x_bar);
-			double stan_dev_y = stan_dev_calc(y_val, y_bar);
-			double corr_coeff = corr_coeff_calc(x_val, x_bar, y_val, y_bar);
-			double slope = slope_calc(corr_coeff, stan_dev_x, stan_dev_y);
-			double y_int = y_int_calc(slope, x_bar, y_bar);
-			
-			System.out.println("Mean x-values: "+x_bar);
-			System.out.println("Mean y-values: "+y_bar);
-			System.out.println("Standard Deviation x-values: "+stan_dev_x);
-			System.out.println("Standard Deviation y-values: "+stan_dev_y);
-			System.out.println("Correlation Coeeficient: "+corr_coeff);
-			System.out.println("Slope: "+slope);
-			System.out.println("y-Intercept: "+y_int);
-			
-			System.out.println("");
-			System.out.println("Enter another sample?");
-			System.out.println("0) No");
-			System.out.println("1) Yes");
-			input1=scan_1.nextInt();
-		}
+		SC1Page mp = new SC1Page();
+		mp.setup();
 	}
-
 }
